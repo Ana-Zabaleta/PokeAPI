1c7631ef2e6b4e459f6c9cb32b9fbc4826808cdd
Ana_Z <113620777+Zana2234@users.noreply.github.com>
2024-03-09 15:41:18 +0100

    Added pokemon JAvaScript PokeAPI

---
+++ index.js
@@ -0,0 +1,64 @@
+// Cogemos el listado del class pokedex
+const pokedex = document.getElementById("pokedex");
+
+// linkar POkeAPI
+const apiUrl = `https://pokeapi.co/api/v2/pokemon?limit=100`;
+
+// Creamos una funcion para cargar los datos que queremos de la PokeAPI
+
+async function cargarDatos(endPoint) {
+  const response = await fetch(endPoint, { method: "GET" });
+  const data = await response.json();
+  if (data.results) {
+    const array = data.results;
+    pintarPokemon(array, sectionPokemon);
+  } else {
+    pintarUnPokemon(data, sectionPokemon);
+  }
+}
+/*const data = await response.json();
+  console.log(data);
+  if (data.results) {
+    //pintar muchos
+    const array = data.results;
+    pintarCharacters(array, sectionCharacters);
+  } else {
+    //pintar uno
+    pintarUnCharacter(data, sectionCharacters);
+  }
+}*/
+cargarDatos(apiUrl);
+//cargarDatos(apiUrl + "/1");
+
+//cargarDatos(apiUrl + "?offset=20&limit=20");
+
+//-------------------------------------------------------
+const sectionPokemon = document.querySelector("#pokedex");
+/* <li class="card">
+          <img class="card-image" src="${pokeman.image}" />
+          <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+          <p class="card-subtitle">Type: ${pokeman.type}</p>
+        </li>*/
+
+function pintarPokemon(lista, lugar) {
+  for (let pokemon of lista) {
+    pintarUnPokemon(pokemon, lugar);
+  }
+}
+async function pintarUnPokemon(character, lugar) {
+  const pokemonUrl = character.url;
+  const pokemon1 = await fetch(pokemonUrl, { method: "GET" });
+  const infoPokemon = await pokemon1.json();
+
+  const li = document.createElement("li"); //<li></li>
+  const img = document.createElement("img"); //<img></img>
+  img.src = infoPokemon.sprites["front_default"];
+  img.alt = infoPokemon.name;
+  const h2 = document.createElement("h2"); //<h2></h2>
+  h2.textContent = infoPokemon.name;
+  const p = document.createElement("p"); //<p></p>
+  p.textContent = `Numero: ${infoPokemon.id}`;
+
+  li.append(img, h2, p);
+  lugar.appendChild(li);
+}
